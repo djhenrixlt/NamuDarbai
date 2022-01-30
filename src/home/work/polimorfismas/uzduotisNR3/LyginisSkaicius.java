4c3fa76904fe4a62ed0e7571c7b53da7a72404e0
djhenrixlt <djhenrixlt@yahoo.com>
2022-01-30 17:53:31 +0100

    added LyginisSkaicius

---
+++ src/home/work/polimorfismas/uzduotisNR3/LyginisSkaicius.java
@@ -0,0 +1,19 @@
+package home.work.polimorfismas.uzduotisNR3;
+
+import java.util.Random;
+
+public class LyginisSkaicius extends Class1{
+    private Random random = new Random();
+    private int low = 1;
+    private int max = 10;
+
+    @Override
+    public void generuok() {
+        int[] d = new int[getSize()];
+        for (int c = 0; c < d.length; c++) {
+            d[c] = random.nextInt(max - low) + low;
+        }
+        setMasivas(d);
+
+    }
+}
